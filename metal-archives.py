4b7ea6edb8342dccb4baf14df31b1b453caa0d26
HeIIow2 <Lars.Noack@outlook.de>
2022-05-19 00:55:10 +0200

    added batch downloading

--- metal-archives.py
+++ metal-archives.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import enum
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 METAL_API_URL = 'https://www.metal-archives.com//search/ajax-advanced/searching/bands/?' \
                 'bandName={bandName}&' \
                 'genre={genre}&' \
@@ -14,7 +15,8 @@ METAL_API_URL = 'https://www.metal-archives.com//search/ajax-advanced/searching/
                 'themes={themes}&' \
                 'location={location}&' \
                 'bandLabelName={bandLabelName}&' \
-                'sEcho=1&iColumns=10&sColumns=0&iDisplayStart={start}&iDisplayLength=200'
+                'sEcho=1&iColumns=10&sColumns=0&iDisplayStart={start}&iDisplayLength={length}'
+
 
 class Band(enum.Enum):
     name = 'name'
@@ -30,6 +32,7 @@ class Band(enum.Enum):
     akronyms = 'akronyms'
     notes = 'notes'
 
+
 def get_metal_data(band_name="*",
                    genre="*",
                    country="*",
@@ -39,68 +42,94 @@ def get_metal_data(band_name="*",
                    status="*",
                    themes=None,
                    location="*",
-                   band_label_name="*"):
+                   band_label_name="*",
+                   length=200,
+                   ):
     bands = []
+    fetched_count = length
 
     if themes is None:
         themes = []
     themes_string = " ".join(themes) if len(themes) > 0 else "*"
 
-    url = METAL_API_URL.format(bandName=band_name, genre=genre, country=country, yearCreationFrom=year_creation_from, yearCreationTo=year_creation_to, bandNotes=band_notes, status=status, themes=themes_string, location=location, bandLabelName=band_label_name, start=0)
-    r = requests.get(url, headers=headers)
-    if int(r.status_code/100) != 2:
-        raise Exception('Error getting metal data: {}'.format(r.status_code))
+    cursor = 0
+    while fetched_count == length:
+        url = METAL_API_URL.format(bandName=band_name, genre=genre, country=country,
+                                   yearCreationFrom=year_creation_from, yearCreationTo=year_creation_to,
+                                   bandNotes=band_notes, status=status, themes=themes_string, location=location,
+                                   bandLabelName=band_label_name, start=cursor, length=length)
+        r = requests.get(url, headers=headers)
+        if int(r.status_code / 100) != 2:
+            raise Exception('Error getting metal data: {}'.format(r.status_code))
 
-    data = r.json()['aaData']
-    for band in data:
-        # band, genre, country, location, themes, year_creation, label, Notes
+        data = r.json()['aaData']
+        fetched_count = len(data)
 
-        print(band)
-        # parse the html for the band name and link on metal-archives
-        soup = BeautifulSoup(band[0], 'html.parser')
-        anchor = soup.find('a')
-        band_name = anchor.text
-        ma_url = anchor.get('href')
-        anchor.decompose()
-        soup.find('strong').decompose()
-        akronyms = soup.text[2:-2].split(', ')
+        if fetched_count == 0:
+            print(cursor)
+            print(url)
+            print(r.json())
 
-        # genre
-        genre = band[1]
+        for band in data:
+            # band, genre, country, location, themes, year_creation, label, Notes
 
-        # country
-        country = band[2]
+            # parse the html for the band name and link on metal-archives
+            soup = BeautifulSoup(band[0], 'html.parser')
+            anchor = soup.find('a')
+            band_name_ = anchor.text
+            ma_url_ = anchor.get('href')
+            anchor.decompose()
+            strong = soup.find('strong')
+            akronyms_ = None
+            if strong is not None:
+                strong.decompose()
+                akronyms_ = soup.text[2:-2].split(', ')
 
-        # location
-        location = band[3]
+            # genre
+            genre_ = band[1]
 
-        # themes
-        themes = band[4].split(',')
+            # country
+            country_ = band[2]
 
-        # year_creation
-        year_creation = int(band[5])
+            # location
+            location_ = band[3]
 
-        # label
-        label = band[6]
+            # themes
+            themes_ = band[4].split(',')
 
-        # notes
-        notes = band[7]
+            # year_creation
+            year_creation_ = None
+            if band[5] != 'N/A':
+                year_creation_ = int(band[5])
 
-        bands.append({
-            Band.name: band_name,
-            Band.akronyms: akronyms,
-            Band.ma_url: ma_url,
-            Band.genre: genre,
-            Band.country: country,
-            Band.location: location,
-            Band.themes: themes,
-            Band.year_creation: year_creation,
-            Band.band_notes: notes,
-            Band.band_label_name: label,
+            # label
+            label_ = band[6]
 
-        })
+            # notes
+            notes_ = band[7]
+
+            bands.append({
+                Band.name: band_name_,
+                Band.akronyms: akronyms_,
+                Band.ma_url: ma_url_,
+                Band.genre: genre_,
+                Band.country: country_,
+                Band.location: location_,
+                Band.themes: themes_,
+                Band.year_creation: year_creation_,
+                Band.band_notes: notes_,
+                Band.band_label_name: label_,
+            })
+        cursor += length
+        print(fetched_count)
 
     return bands
 
+
 if __name__ == "__main__":
-    print(get_metal_data())
+    data = get_metal_data()
+    for band in data:
+        print("")
+        print(band)
+
+    print(len(data))
